c352068fd745331e22703bd2b07e7a3671e5e5db
Edy <daicueduard@gmail.com>
2024-12-21 16:54:00 +0200

    Project commit

---
+++ Waveform.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace waveform
+{
+    partial class Waveform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpDown = new System.Windows.Forms.Button();
+            this.labelUpDown = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUpDown
+            // 
+            this.btnUpDown.Location = new System.Drawing.Point(58, 64);
+            this.btnUpDown.Name = "btnUpDown";
+            this.btnUpDown.Size = new System.Drawing.Size(150, 50);
+            this.btnUpDown.TabIndex = 0;
+            this.btnUpDown.Text = "Up - Down";
+            this.btnUpDown.UseVisualStyleBackColor = true;
+            this.btnUpDown.Click += new System.EventHandler(this.btnUpDown_Click);
+            // 
+            // labelUpDown
+            // 
+            this.labelUpDown.AutoSize = true;
+            this.labelUpDown.Location = new System.Drawing.Point(242, 77);
+            this.labelUpDown.Name = "labelUpDown";
+            this.labelUpDown.Size = new System.Drawing.Size(394, 25);
+            this.labelUpDown.TabIndex = 1;
+            this.labelUpDown.Text = "Apasă butonul pentru a începe afișarea.";
+            this.labelUpDown.UseMnemonic = false;
+            // 
+            // Waveform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelUpDown);
+            this.Controls.Add(this.btnUpDown);
+            this.Name = "Waveform";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Waveform_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpDown;
+        private System.Windows.Forms.Label labelUpDown;
+    }
+}
+
---
+++ Waveform.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace waveform
+{
+    public partial class Waveform : Form
+    {
+        private int val = 0;
+        private const int valMax = 100;
+        private const int increment = 10;
+        private int x = 10;
+        private bool check = true;
+        public Waveform()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpDown_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                val += increment;
+                if (val >= valMax)
+                {
+                    val = valMax;
+                    check = false;
+                }
+            }
+            else
+            {
+                val -= increment;
+                if (val <= 0)
+                {
+                    val = 0;
+                    check = true;
+                }
+            }
+
+            labelUpDown.Text = val.ToString();
+            DrawBar();
+        }
+
+        private void DrawBar()
+        {
+            Graphics draw = this.CreateGraphics();
+            Brush barBrush = new SolidBrush(Color.Blue);
+            int barWidth = 5;
+            int barHeight = val + 10;
+            int y = this.Height - barHeight - 50;
+
+            draw.FillRectangle(barBrush, x, y, barWidth, barHeight);
+
+            x += barWidth + 5;
+
+            draw.Dispose();
+        }
+
+        private void Waveform_Load(object sender, EventArgs e)
+        {
+            this.Text = "L2 - Daicu Eduard - IMD1";
+            this.Width = 900;
+            this.Height = 300;
+        }
+    }
+}
